add01dd316da489a1c82636d50cf74f0ae50f6cc
yaotutu <957810668@qq.com>
2024-09-09 14:07:20 +0800

    ✨ 新功能(layout.tsx)：添加Navbar组件以增强仪表板布局 ✨ 新功能(Navbar.tsx)：创建Navbar组件以提供搜索和用户信息功能

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "../../components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -22,7 +23,7 @@ export default function DashboardLayout({
       </div>
       {/* RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
       </div>
     </div>
